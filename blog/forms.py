6efdb6f0bb31dff6c2a03b4d619cfccb4041f4cc
Ibrahim Rahimi <irahimi@netlinks.af>
2020-09-28 22:36:46 +0430

    Add create post form template.

--- blog/forms.py
+++ blog/forms.py
@@ -57,3 +57,4 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    cancel = SubmitField('Cancel')
