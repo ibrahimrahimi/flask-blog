7c30be4478721f04cfc95c5e5edd41d28ca5739e
Ibrahim Rahimi <irahimi@netlinks.af>
2020-09-29 15:33:49 +0430

    Add view one post feature.

--- blog/routes.py
+++ blog/routes.py
@@ -107,3 +107,8 @@ def create_post():
         flash('Post has been created successfully!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
