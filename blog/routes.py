0d9acfb8aca2e45cdd5c74083d8fc7f26752d40c
Ibrahim Rahimi <irahimi@netlinks.af>
2020-09-30 23:58:23 +0430

    Add pagination for displaying posts and show latest post first.

--- blog/routes.py
+++ blog/routes.py
@@ -15,7 +15,8 @@ db.session.commit()
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
