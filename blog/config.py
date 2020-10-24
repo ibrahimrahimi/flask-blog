fb9f72b21d64e33ee87a6540dc5bbd012b9942cd
Ibrahim Rahimi <irahimi@netlinks.af>
2020-10-24 10:38:58 +0430

    Update config.py file.

--- blog/config.py
+++ blog/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = 'smtp.googlemail.com'
